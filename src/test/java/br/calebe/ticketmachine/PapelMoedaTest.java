11b092f2ca4872fcc4016d08a0566d2af85d323a
Lucas Gomes Trevizoli <99925705+lukinhasgomes@users.noreply.github.com>
2024-04-24 22:47:21 -0300

    Criação de teste da classe papelMoeda
    teste de valores diferentes

--- src/test/java/br/calebe/ticketmachine/PapelMoedaTest.java
+++ src/test/java/br/calebe/ticketmachine/PapelMoedaTest.java
@@ -1,5 +1,30 @@
+
 package br.calebe.ticketmachine;
 
+import static org.junit.Assert.*;
+import br.calebe.ticketmachine.core.PapelMoeda;
+import org.junit.Test;
+
 public class PapelMoedaTest {
-    
+
+    @Test
+    public void testQuantidadeDiferente() {
+        PapelMoeda papelMoeda = new PapelMoeda(5, 10);
+        assertEquals(10, papelMoeda.getQuantidade());
+    }
+
+    @Test
+    public void testValorDiferente() {
+        PapelMoeda papelMoeda = new PapelMoeda(20, 5);
+        assertEquals(20, papelMoeda.getValor());
+    }
+
+    @Test
+    public void testValorEQuantidadeDiferente() {
+        PapelMoeda papelMoeda = new PapelMoeda(10, 10);
+        assertEquals(10, papelMoeda.getQuantidade());
+        assertEquals(10, papelMoeda.getValor());
+    }
+
+
 }
